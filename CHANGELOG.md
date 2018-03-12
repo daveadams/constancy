905092887bfd6c6481084dc1b021b7dbafd4fca0
David Adams <dadams@instructure.com>
2018-03-12 15:10:50 -0500

    Bump version to 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1 (March 12, 2018)
+
+*BUG*: Fix Ruby syntax fail for failthru of default values (#1)
+
 ## 0.1.0 (February 1, 2018)
 
 Initial release
--- lib/constancy/version.rb
+++ lib/constancy/version.rb
@@ -1,5 +1,5 @@
 # This software is public domain. No rights are reserved. See LICENSE for more information.
 
 class Constancy
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
